ea3490c78a9a6f542db618ec0cdc20bd39766aa4
Marcelo <mhnbrito6@gmail.com>
2026-03-03 20:58:33 -0300

    Initial commit

---
+++ src/main/java/com/marcelo/sistemaplanodesaude/SistemaplanodesaudeApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.sistemaplanodesaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaplanodesaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaplanodesaudeApplication.class, args);
+	}
+
+}
