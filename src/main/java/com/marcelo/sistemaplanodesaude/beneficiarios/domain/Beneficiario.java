cef7cdb9d6a752c135b00ddffdfc210f755e58a1
Marcelo <mhnbrito6@gmail.com>
2026-03-04 20:54:14 -0300

    Mapeamento da entidade beneficiário.

---
+++ src/main/java/com/marcelo/sistemaplanodesaude/beneficiarios/domain/Beneficiario.java
@@ -0,0 +1,36 @@
+package com.marcelo.sistemaplanodesaude.beneficiarios.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "beneficiarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beneficiario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+    @Column(length = 11, nullable = false)
+    private String telefone;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "data_inclusao", nullable = false)
+    private LocalDate dataInclusao;
+
+    @Column(name = "data_atualizacao", nullable = false)
+    private LocalDate dataAtualizacao;
+}
