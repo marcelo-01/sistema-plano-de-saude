f2b4f2240056ed0d02c360f1d46295a61d03cbb7
Marcelo <mhnbrito6@gmail.com>
2026-03-13 12:03:11 -0300

    Criado DTO para listar todos os beneficiários.

---
+++ src/main/java/com/marcelo/sistemaplanodesaude/beneficiarios/dto/BeneficiarioListResponse.java
@@ -0,0 +1,35 @@
+package com.marcelo.sistemaplanodesaude.beneficiarios.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.marcelo.sistemaplanodesaude.beneficiarios.domain.Beneficiario;
+import com.marcelo.sistemaplanodesaude.documentos.domain.Documento;
+import com.marcelo.sistemaplanodesaude.documentos.dto.DocumentoResponse;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+@JsonPropertyOrder({"id", "nome", "telefone", "dataNascimento", "documentos"})
+public class BeneficiarioListResponse {
+    private UUID id;
+    private String nome;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private List<DocumentoResponse> documentos;
+
+    public BeneficiarioListResponse(Beneficiario beneficiario){
+        this.id = beneficiario.getId();
+        this.nome = beneficiario.getNome();
+        this.telefone = beneficiario.getTelefone();
+        this.dataNascimento = beneficiario.getDataNascimento();
+        this.documentos = beneficiario.getDocumentos()
+                .stream()
+                .map(DocumentoResponse::new)
+                .collect(Collectors.toList());
+    }
+}
